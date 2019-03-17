dbc49531fdf93a7ae7af2cfb9ab2cac233f08c74
“Julie-S-Bernal“ <Julies.bernal@gmail.coml>
2019-03-17 18:20:30 +0000

    trying out animations

--- src/Profile/index.js
+++ src/Profile/index.js
@@ -21,17 +21,19 @@ class Profile extends Component {
           <p className="profileText">Hi, my name is Julie Bernal and I am a Web Developer.</p>
           <p className="profileText">I design sharp interfaces to create the best user experience.</p>
           <p className="profileText">I code with React, React Native, Redux and Node.js</p>
-          <div className="col-12">
-          <i class="fas fa-chevron-right"  onClick={this.handleShowNextpage}></i>
+          <div className="col-12 chevronPosition">
+          <i class="fas fa-chevron-right chevronStyle "  onClick={this.handleShowNextpage}></i>
           </div>
           </div>
       )
     } return(
-      <div className="rowContainer  textContainer">
-      <h1>Bio</h1>
-      <p>incoming...</p>
-      <div className="col-4">
-      <i class="fas fa-chevron-left" onClick={this.handleShowPreviousPage}></i>
+     <div className="rowContainer textContainer">
+          <h1> About Me</h1>
+          <p className="profileText">Hi, my name is Julie Bernal and I am a Web Developer.</p>
+          <p className="profileText">I design sharp interfaces to create the best user experience.</p>
+          <p className="profileText">I code with React, React Native, Redux and Node.js</p>
+      <div className="col-12 chevronPosition">
+      <i class="fas fa-chevron-left chevronStyle" onClick={this.handleShowPreviousPage}></i>
       </div>
       </div>
     )
